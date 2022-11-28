219798da1639066a14f407a804c7c861dbd6def4
Daniel Rosenberg <drosen@google.com>
2022-11-28 16:09:23 +0000

    X6882: Enable Project ID Quota support on userdata.
     * These features enable wide inodes on userdata, which
       is required for project ID quotas. Wide inodes can
       only be set at MKFS time.
    
     * This also enables casefolding, which is required for the
       removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -70,6 +70,9 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
