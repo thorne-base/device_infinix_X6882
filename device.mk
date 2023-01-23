0c52fa3b98b50ca6ca64e8669775cc7d438cd55e
Jaroslav Zviezda <acroreiser@gmail.com>
2023-01-24 01:59:15 +0800

    X6882: Enable DM file pre-opting to reduce first boot time
    Change-Id: If8080682d7c2eaf1701e54cccf596f1fd13b03cd
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -247,6 +247,9 @@ $(call inherit-product-if-exists, vendor/JamesDSP/config.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/mtk-tpd.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/mtk-tpd.kl
 
+# Enable DM file pre-opting to reduce first boot time
+PRODUCT_DEX_PREOPT_GENERATE_DM_FILES := true
+
 # Keymaster
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
