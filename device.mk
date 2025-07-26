666beb25e0bebace828e8a04ac675cd2b34aca39
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2025-07-26 23:07:50 +0900

    X6882: Configure Sensor Stack
    Refs: https://github.com/PQEnablers-Devices/android_device_xiaomi_ruby/commit/3bf7b31611ab1146f605371790d9499a70af4dd8
    
    Co-authored-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I5ba38325862712bb8666b936ea7d4e395f46f6b9
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -154,9 +154,6 @@ PRODUCT_PACKAGES += \
     init.sensor_2_0.rc \
     ueventd.mt6789.rc
 
-# Shipping Api levels
-PRODUCT_SHIPPING_API_LEVEL := 31
-
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
@@ -219,6 +216,21 @@ PRODUCT_COPY_FILES += \
 
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Sensors
+PRODUCT_PACKAGES += \
+    libsensorndkbridge \
+    android.hardware.sensors@1.0.vendor \
+    android.hardware.sensors@2.1.vendor \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
+# Shipping Api levels
+PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
