89d11b9c76211b598bc4382ae19e48f2f551324c
LuK1337 <priv.luk@gmail.com>
2025-08-05 22:52:03 +0900

    X6882: overlay: import settings provider
    * this used to set global device name,
       also XOS uses ro.product.product.tran.device.name.default for default device name.
    
    Change-Id: I310c1ffc581b72653783d1093d9e7ebde544bb16
    Signed-off-by: dodyirawan85 <dev.irawans@gmail.com>
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -313,6 +313,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworkResOverlayNoir \
     SettingsResOverlayNoir \
+    SettingsProviderOverlayNoir \
     SystemUIResOverlayNoir \
     TetheringResOverlayNoir \
     WifiResOverlayNoir
