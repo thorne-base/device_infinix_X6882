db12ae89d1fd1349d94aed34d2d169771e47ae79
Mashopy <eliasgheeraert@gmail.com>
2026-03-07 03:41:32 +0800

    mt6789-common: Inherit MSSI common overlays
    Change-Id: I9fdb37b77f0a6e4e6afb7488328a3485df4a7cb4
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -308,14 +308,13 @@ PRODUCT_PACKAGES += \
     Tag
 
 # Overlays
+$(call inherit-product, hardware/mediatek/overlay/mssi.mk)
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworkResOverlayNoir \
     SettingsResOverlayNoir \
     SettingsProviderOverlayNoir \
-    SystemUIResOverlayNoir \
-    TetheringResOverlayNoir \
-    WifiResOverlayNoir
+    SystemUIResOverlayNoir
 
 # Lineage-Specific Overlays
 PRODUCT_PACKAGES += \
@@ -411,6 +410,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Radio
+ENABLE_VENDOR_RIL_SERVICE := true
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor:64 \
     android.hardware.radio@1.6.vendor:64
