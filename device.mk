6c4e5cc7a5d704dd807114dd2d58ed5cb424dbd2
basamaryan <basam.aryan@gmail.com>
2026-07-02 13:51:56 +0800

    X6882: Switch to common MTK USB gadget init
    Change-Id: I6dc9b96c3b0edb7bfe2993843bacba24a51910ed
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- device.mk
+++ device.mk
@@ -233,7 +233,6 @@ PRODUCT_PACKAGES += \
     init.modem.rc \
     init.mt6789.power.rc \
     init.mt6789.rc \
-    init.mt6789.usb.rc \
     init.mtkgki.rc \
     init.project.rc \
     init.recovery.usb.rc \
@@ -504,7 +503,8 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set_bool,android_hardware_mediatek_usb,audio_accessory_supported,true)
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.mediatek \
-    android.hardware.usb.gadget-service.mediatek
+    android.hardware.usb.gadget-service.mediatek \
+    init.mt6789.usb.rc
 
 # userdata
 PRODUCT_FS_COMPRESSION := 1
