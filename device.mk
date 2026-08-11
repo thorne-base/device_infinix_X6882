b483543926befeede8beb91d30502f2a153c0546
gutssnv <decryptor69@gmail.com>
2026-08-12 02:12:36 +0700

    X6882: clear out device.mk and add hw/transsion to soong
    Change-Id: I6220c279b69887de95d88d61d39b4f650a96be71
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -347,18 +347,19 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek/libmtkperf_client \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/google/interfaces \
-    hardware/google/pixel
+    hardware/google/pixel \
+    hardware/transsion
 
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.mediatek
-    
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
-    
+
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.transsion    
+    android.hardware.vibrator-service.transsion
 
 # VNDK
 PRODUCT_PACKAGES += \
